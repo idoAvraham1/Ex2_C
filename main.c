06888ca2cf03067da8e19072c07ba5ccfd605f8c
idoAvraham1 <155999367+idoAvraham1@users.noreply.github.com>
2024-02-02 05:19:06 +0200

    checking

--- main.c
+++ main.c
@@ -1,46 +1,32 @@
-
-#include <stdio.h>
-#include "my_mat.h"
-#define N 10
 int main() {
     int graph[N][N];
     int dist[N][N];
+    int i, j;
+    char options;
 
-    int i=0;
-    int j=0;
-    char options='o';
+    do {
+        scanf(" %c", &options); 
 
-   do {
-        scanf("%c", &options);
-        if(options =='A'){
+        if (options == 'A') {
             initMatrix(graph);
-        }
-        else if(options=='B'){
-            scanf("%d %d ", &i, &j);
-            shortestPath( graph, dist);
-            if(dist[i][j]==0){
-                printf("False \n");
+        } else if (options == 'B') {
+            scanf("%d %d", &i, &j);
+            shortestPath(graph, dist);
+            if (dist[i][j] == 0) {
+                printf("False\n");
+            } else {
+                printf("True\n");
             }
-            else{
-                printf("True \n");
+        } else if (options == 'C') {
+            scanf("%d %d", &i, &j);
+            shortestPath(graph, dist);
+            if (dist[i][j] == 0) {
+                printf("-1\n");
+            } else {
+                printf("%d\n", dist[i][j]);
             }
         }
+    } while (options != 'D');
 
-
-        else if(options=='C'){
-            scanf("%d %d ", &i, &j);
-            shortestPath( graph, dist);
-            if(dist[i][j]==0){
-                printf("-1 \n");
-            }
-            else{
-                printf("%d \n",dist[i][j]);
-            }
-        }
-        if (options=='D'){
-            return 0;
-        }
-
-    }
-     while(options!='D');
+    return 0;
 }
