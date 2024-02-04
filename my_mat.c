2bb662a18161a67aaa99712eac6bb47ecd9ae2c5
idoAvraham1 <155999367+idoAvraham1@users.noreply.github.com>
2024-02-04 20:25:24 +0200

    ido

--- my_mat.c
+++ my_mat.c
@@ -1,30 +1,29 @@
-#include <stdio.h>
 #include "my_mat.h"
+#include <stdio.h>
 #include <math.h>
 #define N 10
 
- void shortestPath(int graph[N][N], int dist[N][N]) {
-    // Copy the input graph to the distance matrix
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+ void shortestPath(int graph[N][N],int dist[N][N]){
+
+     for (int i = 0; i < N; i++) {
+         for (int j = 0; j < N; j++) {
             dist[i][j] = graph[i][j];
         }
-    }
-
-    // Floyd-Warshall algorithm
-    for (int k = 0; k < N; k++) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (dist[i][k] != 0 && dist[k][j] != 0) {
-                    if (dist[i][j] == 0 || dist[i][j] > dist[i][k] + dist[k][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
-                }
-            }
-        }
-    }
-}
-
+     }
+     for (int k = 0; k < N; k++) {
+         for (int i = 0; i < N; i++) {
+             for (int j = 0; j < N; j++) {
+                 if (i != j) {
+                     if (dist[i][k] != 0 &&  dist[k][j] != 0) {
+                         if (dist[i][j] == 0 || dist[i][j] > dist[i][k] + dist[k][j]) {
+                             dist[i][j] = dist[i][k] + dist[k][j];
+                         }
+                     }
+                 }
+             }
+         }
+     }
+ }
 void initMatrix(int matrix[N][N]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
