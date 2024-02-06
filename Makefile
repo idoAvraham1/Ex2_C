609ecc8c7dc97dc613a4654d0412ba84fc5c5b95
idoAvraham1 <155999367+idoAvraham1@users.noreply.github.com>
2024-02-06 19:42:22 +0200

    checking

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = gcc
 CFLAGS = -Wall -g
 OBJ = my_mat.o
 
-all: connections
+all: connections my_Knapsack
 
 # Create shared library with my_mat
 libmy_mat.so: $(OBJ) my_mat.h
@@ -16,6 +16,9 @@ connections: main.o libmy_mat.so
 main.o: main.c my_mat.h
 	$(CC) $(CFLAGS) -c main.c
 
+my_Knapsack: my_Knapsack.o
+	$(CC) $(CFLAGS) my_Knapsack.o -o my_Knapsack
+
 # Clean all
 clean:
-	rm -f *.o *.so connections
+	rm -f *.o *.so connections my_Knapsack
---
+++ my_Knapsack.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MAXW 20
+#define ITEMS 5
+
+void ChooseItems(int weights[], int values[], int profitT[][MAXW+1], int selected_bool[]){
+    int i=ITEMS;
+    int j= MAXW;
+     for( int x=0; x<ITEMS;x++){
+         selected_bool[x]=0;
+     }
+
+   while (i > 0 && j > 0) {
+        int w_i=weights[i-1];
+
+
+        if(profitT[i][j]>profitT[i-1][j]){
+            selected_bool[i-1]=1;
+            i=i-1;
+            j=j-w_i;
+        }
+        else{
+            i=i-1;
+        }
+    }
+}
+
+
+void MaxProfit(int weights[ITEMS], int values[], int profitT[][MAXW+1]) {
+   for (int i = 0; i <= ITEMS; ++i) {
+       for (int j = 0; j <= MAXW; j++) {
+           if (j == 0 || i == 0) {
+               profitT[i][j] = 0;
+           }
+       }
+   }
+   for (int i = 1; i <= ITEMS; i++) {
+       for (int j = 1; j <= MAXW; j++) {
+           if (weights[i - 1] <= j) {
+               profitT[i][j] = MAX(profitT[i - 1][j],
+                                   values[i - 1] + profitT[i - 1][j - weights[i - 1]]);
+           } else {
+               profitT[i][j] = profitT[i - 1][j];
+           }
+       }
+   }
+}
+int KnapSack(int weights[], int values[],int selected_bool[]){
+   int profitT[ITEMS+1][MAXW+1] = {0};
+   MaxProfit(weights,values,profitT);
+      ChooseItems(weights,values, profitT,selected_bool);
+    return profitT[ITEMS][MAXW];
+}
+void ScanArrays(char items[], int values[], int weights[]) {
+    for (int i = 0; i < ITEMS; ++i) {
+        // Input for items array
+        //printf("Enter item: ");
+        scanf(" %c", &items[i]);
+    }
+
+    for (int i = 0; i < ITEMS; ++i) {
+        // Input for values array
+        //printf("Enter value for item %c: ", items[i]);
+        scanf("%d", &values[i]);
+    }
+
+    for (int i = 0; i < ITEMS; ++i) {
+        // Input for weights array
+        //printf("Enter weight for item %c: ", items[i]);
+        scanf("%d", &weights[i]);
+    }
+}
+
+
+void PrintItems(int selected_bool[], char items[]) {
+   printf("Items that give the maximum profit: [");
+   for (int i = 0; i < ITEMS; ++i) {
+
+       if (selected_bool[i] == 1) {
+           printf("%c,", items[i]);
+       }
+   }
+   printf("]\n");
+}
+
+
+
+int main() {
+   char items[ITEMS];
+   int values[ITEMS];
+   int weights[ITEMS];
+   int selected_bool[ITEMS];
+
+
+   // Scan arrays from the user
+   ScanArrays(items, values, weights);
+
+
+   // Call KnapSack to calculate maximum profit and selected items
+   int maxProfit = KnapSack(weights, values, selected_bool);
+
+
+   // Print the maximum profit
+   printf("Maximum profit is: %d\n", maxProfit);
+
+
+   // Print the selected items
+   printf("Items that give the maximum profit: [");
+   for (int i = 0; i < ITEMS; ++i) {
+       if (selected_bool[i] == 1) {
+           printf("%c , ", items[i]);
+       }
+   }
+   printf("]\n");
+
+
+   return 0;
+}
+
